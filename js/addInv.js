342bb0cc666a9f52c569f25f42d61053c1956061
ru-sss <53507400+ru-sss@users.noreply.github.com>
2020-08-09 21:56:53 +0900

    a

--- js/addInv.js
+++ js/addInv.js
@@ -5,24 +5,25 @@
 
     if(opt!=="0"){
     	$("#"+opt).show();
-    	
     } else {
-    	$("#inputMType").change(function(){
-
-	    	if(opt == "0"){
-	    		$("#ifOpt1").hide();
-	    		$("#ifOpt2").hide();
-	    	} else {
-	    		if(opt=="ifOpt1"){
-		    		$("#"+opt).slideDown("slow");
-		    		$("#ifOpt2").hide();
-			    } else if(opt=="ifOpt2"){
-			    	$("#"+opt).slideDown("slow");
-		    		$("#ifOpt1").hide();
-			    }
-	    	}
-	    	
-	    });
+    	
     }    
+
+    $("#inputMType").change(function(){
+
+	    if(opt == "0"){
+	    	$("#ifOpt1").hide();
+	    	$("#ifOpt2").hide();
+	    } else {
+	    	if(opt=="ifOpt1"){
+		    	$("#"+opt).slideDown("slow");
+		    	$("#ifOpt2").hide();
+			} else if(opt=="ifOpt2"){
+			    $("#"+opt).slideDown("slow");
+		    	$("#ifOpt1").hide();
+			}
+	    }
+	    	
+	});
 
 })(jQuery);
