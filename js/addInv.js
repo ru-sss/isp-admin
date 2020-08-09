f7fdf4e5f9b00f724f80386c723d3afca865aa78
ru-sss <53507400+ru-sss@users.noreply.github.com>
2020-08-09 21:36:19 +0900

    a

--- js/addInv.js
+++ js/addInv.js
@@ -3,20 +3,7 @@
 
     var opt = $("#inputMType option:selected").val();
 
-    if(opt == "0"){
-    	$("#ifOpt1").hide();
-    	$("#ifOpt2").hide();
-    } else {
-    	if(opt=="ifOpt1"){
-	    	$("#"+opt).show();
-	    	$("#ifOpt2").hide();
-		} else if(opt=="ifOpt2"){
-			$("#"+opt).show();
-	    	$("#ifOpt1").hide();
-		}
-   	}
-
-    $("#inputMType").change(function(){
+	$("#inputMType").change(function(){
     	
     	if(opt == "0"){
     		$("#ifOpt1").hide();
@@ -31,7 +18,22 @@
 		    }
     	}
     	
-    })
+    });
+
+    if(opt == "0"){
+    	$("#ifOpt1").hide();
+    	$("#ifOpt2").hide();
+    } else {
+    	if(opt=="ifOpt1"){
+	    	$("#"+opt).show();
+	    	$("#ifOpt2").hide();
+		} else if(opt=="ifOpt2"){
+			$("#"+opt).show();
+	    	$("#ifOpt1").hide();
+		}
+   	}
+
+    
     
 
 })(jQuery);
