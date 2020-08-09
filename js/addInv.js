e42cf996f8f3091644b9ff47ac91561d88ec8515
ru-sss <53507400+ru-sss@users.noreply.github.com>
2020-08-09 21:34:10 +0900

    a

--- js/addInv.js
+++ js/addInv.js
@@ -1,8 +1,22 @@
 (function($) {
     "use strict";
 
+    var opt = $("#inputMType option:selected").val();
+
+    if(opt == "0"){
+    	$("#ifOpt1").hide();
+    	$("#ifOpt2").hide();
+    } else {
+    	if(opt=="ifOpt1"){
+	    	$("#"+opt).show();
+	    	$("#ifOpt2").hide();
+		} else if(opt=="ifOpt2"){
+			$("#"+opt).show();
+	    	$("#ifOpt1").hide();
+		}
+   	}
+
     $("#inputMType").change(function(){
-    	var opt = $("#inputMType option:selected").val();
     	
     	if(opt == "0"){
     		$("#ifOpt1").hide();
