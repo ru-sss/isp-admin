c4dff80c41cc54e529d46a2282aca16841a720d1
ru-sss <53507400+ru-sss@users.noreply.github.com>
2020-08-10 21:44:08 +0900

    a

--- js/addInv.js
+++ js/addInv.js
@@ -1,43 +1,27 @@
 (function($) {
     "use strict";
 
+    var opt = $("#inputMType option:selected").val();
+
+    if(opt !== "0") {
+    	$("#"+opt).show();
+    }
+
     $("#inputMType").change(function(){
 
     	var opt = $("#inputMType option:selected").val();
-    	
-    	console.log(opt);
 
-    	if(opt !== "0") {
-    		console.log(opt);
-
+    	if(opt == "ifOpt1") {
     		$("#"+opt).slideDown("slow");
+    		$("#ifOpt2").hide();
+    	} else if(opt == "ifOpt2") {
+    		$("#"+opt).slideDown("slow");
+    		$("#ifOpt1").hide();
     	} else {
     		$("#ifOpt1").hide();
 	    	$("#ifOpt2").hide();
-    	}
-
-
-/*	    if(opt == "0"){
-	    	$("#ifOpt1").hide();
-	    	$("#ifOpt2").hide();
-	    } else {
-	    	if(opt=="ifOpt1"){
-		    	$("#"+opt).slideDown("slow");
-		    	$("#ifOpt2").hide();
-			} else if(opt=="ifOpt2"){
-			    $("#"+opt).slideDown("slow");
-		    	$("#ifOpt1").hide();
-			}
-	    }*/
-	    	
+	    }	
 	});
-
-/*    if(opt!=="0"){
-    	$("#"+opt).show();
-    } else {
-    	
-    }    
-*/
 
 
 })(jQuery);
